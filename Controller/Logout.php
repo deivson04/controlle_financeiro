7ed924db740a995131c9e4daa125491bc9764ea0
Deivson <araujodeivson@gmail.com>
2026-03-07 09:43:46 -0300

    Fix: Ajuste no gitiginore e config

--- Controller/Logout.php
+++ Controller/Logout.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../Config/config.php';
+require_once __DIR__ . '/../Config/Config.php';
 
 session_start();
 session_destroy();
-header("Location: /index.php");
+header("Location: " . BASE_URL . "index.php");
 exit();
